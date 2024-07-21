a3f8b43b4d7bef410181ce8758c6793c62ea8b31
Focuslinkstech <45756999+Focuslinkstech@users.noreply.github.com>
2024-07-21 16:20:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -290,7 +290,7 @@ case $n in
     #sudo ln -s ../mods-available/sqlcounter sqlcounter
     #cd
     sudo ln -s "$freeradius_config_dir/mods-available/sql" "$freeradius_config_dir/mods-enabled/"
-    #sudo ln -s "$freeradius_config_dir/mods-available/sqlcounter" "$freeradius_config_dir/mods-enabled/"
+    sudo mv $freeradius_config_dir/radiusd.conf $freeradius_config_dir/radiusd.conf.back
     sudo mv $freeradius_config_dir/sites-available/default $freeradius_config_dir/sites-available/default.back
     sudo cp ~/freeradius-config/config-files/default $freeradius_config_dir/sites-available/default
     sudo mv $freeradius_config_dir/sites-available/inner-tunnel $freeradius_config_dir/sites-available/inner-tunnel.back
@@ -299,6 +299,7 @@ case $n in
     sudo cp ~/freeradius-config/config-files/sql $freeradius_config_dir/mods-available/sql
     sudo mv $freeradius_config_dir/mods-available/sqlcounter $freeradius_config_dir/mods-available/sqlcounter.back
     sudo cp ~/freeradius-config/config-files/sqlcounter $freeradius_config_dir/mods-available/sqlcounter
+    sudo cp ~/freeradius-config/config-files/radiusd.conf $freeradius_config_dir/radiusd.conf
     sudo cp -r ~/freeradius-config/config-files/mysql/* $freeradius_config_dir/mods-config/sql/counter/mysql/
     sudo sed -i 's|driver = "rlm_sql_null"|driver = "rlm_sql_mysql"|' "$freeradius_config_dir/mods-available/sql"
     sudo sed -Ei '/^[\t\s#]*tls\s+\{/, /[\t\s#]*\}/ s/^/#/' "$freeradius_config_dir/mods-available/sql"
